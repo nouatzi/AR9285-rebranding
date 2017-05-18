0a3f84cc197764e52cf19633cb1e9be2f8c28c67
nouatzi <nouatzi@hotmail.com>
2017-05-18 10:30:36 +0200

    ras...

---
+++ empty/MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+import UIKit
+//import Photos
+//import PlaygroundSupport
+//import MediaPlayer
+
+let string: NSAttributedString = NSAttributedString(string: "cocoa allo pipi", attributes: [NSBackgroundColorAttributeName:UIColor.red])
+
+let label: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 40))
+label.attributedText = string
+
+let view: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 40))
+view.backgroundColor = UIColor.white
+//let url = URL(string: "ipod-library://item/item.mp3?id=9020924975138803651")!
+//let string = url.absoluteString
+//let reurl = URL(string: string)
+
+//MPMediaType.anyVideo.rawValue
+//PHAssetMediaType.image.rawValue
+
+//let string: NSString = "Hello, Cocoa!"
+//let selector = #selector(NSString.lowercased(with:))
+//let locale = Locale.current
+//if let result = string.perform(selector, with: locale) {
+//  print(result.takeUnretainedValue())
+//}
+
+
+
+//MPMediaGrouping.podcastTitle.rawValue
+
+//PlaygroundPage.current.needsIndefiniteExecution = true
+// 18446744073709551615
+//var mm: Int64 = 0
+//print(MPMediaType.any.rawValue)
+
+//mm  = Int64(MPMediaType.any.rawValue)
